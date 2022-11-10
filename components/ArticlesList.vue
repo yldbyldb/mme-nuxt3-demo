00bffc7e50bb5b019eed356493c4aee0b37760ca
Bruce Dong <bruce.dong@societyone.com.au>
2022-11-11 09:26:34 +1100

    try to use contentful plugin and apollo with rc.11

--- components/ArticlesList.vue
+++ components/ArticlesList.vue
@@ -1,23 +1,45 @@
-<script setup>
-const { $contentfulClient } = useNuxtApp()
-const { data } = await useAsyncData('entries', async () => {
-  return await $contentfulClient.getEntries({
-    limit: 10,
-  })
-})
-console.log('====================================');
-console.log(data.value.items);
-console.log('====================================');
-const items = data.value.items
-
-// const { $assertion } = useNuxtApp()
-// $assertion.assertEntries(items)
-
-</script>
-
 <template>
-
-  <h2>{{items[0].fields.content}}</h2>
+  <p>{{data}}</p>
+  <!-- <p v-for="(item, i) in items " :key="i">{{item.slug}}</p> -->
 </template>
+
+<script lang="ts" setup>
+// const { $contentfulClient } = useNuxtApp()
+// const { data } = await useAsyncData('entries', async () => {
+//   return await $contentfulClient.getEntries({
+//     limit: 10,
+//   })
+// })
+// console.log('====================================');
+// console.log(data.value.items);
+// console.log('====================================');
+// const items = data.value.items
+
+// // const { $assertion } = useNuxtApp()
+// $assertion.assertEntries(items)
+// const query = gql`
+//   query getShips($limit: Int!) {
+//     ships(limit: $limit) {
+//       id
+//       name
+//     }
+//   }
+// `
+const query = gql`
+  query getPL {
+    pagenewUiCollection {
+      items {
+        page
+      }
+    }
+  }
+`
+const  data  = await useAsyncQuery(query, { limit: 2 })
+// const items = data.pagepersonalLoanCollection.items
+
+console.log('====================================');
+console.log(data + "!!!");
+console.log('====================================');
+</script>
 
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo'],
 
   runtimeConfig: {
     private: {
@@ -10,6 +10,15 @@ export default defineNuxtConfig({
     },
     public: {
 
+    },
+  },
+  
+  apollo: {
+    clients: {
+      default: {
+        // httpEndpoint: `https://api.spacex.land/graphql`
+        httpEndpoint: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/explore?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
+      }
     },
   },
 
