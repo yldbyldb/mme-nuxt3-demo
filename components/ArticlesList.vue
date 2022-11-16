8f6642cda57570abd3c82b622558ef1e36eb076f
Bruce Dong <bruce.dong@societyone.com.au>
2022-11-16 15:52:49 +1100

    fix the connection issues FINALLY

--- components/ArticlesList.vue
+++ components/ArticlesList.vue
@@ -1,30 +1,10 @@
 <template>
-  <p>{{data}}</p>
+  <div>{{data}}</div>
   <!-- <p v-for="(item, i) in items " :key="i">{{item.slug}}</p> -->
 </template>
 
 <script lang="ts" setup>
-// const { $contentfulClient } = useNuxtApp()
-// const { data } = await useAsyncData('entries', async () => {
-//   return await $contentfulClient.getEntries({
-//     limit: 10,
-//   })
-// })
-// console.log('====================================');
-// console.log(data.value.items);
-// console.log('====================================');
-// const items = data.value.items
 
-// // const { $assertion } = useNuxtApp()
-// $assertion.assertEntries(items)
-// const query = gql`
-//   query getShips($limit: Int!) {
-//     ships(limit: $limit) {
-//       id
-//       name
-//     }
-//   }
-// `
 const query = gql`
   query getPL {
     pagenewUiCollection {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,14 @@
+import { defineNuxtConfig } from 'nuxt/config'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/apollo'],
 
-  runtimeConfig: {
-    private: {
-      CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-      CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-    },
-    public: {
-
-    },
-  },
-  
   apollo: {
     clients: {
       default: {
-        // httpEndpoint: `https://api.spacex.land/graphql`
-        httpEndpoint: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/explore?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`
-      }
+        httpEndpoint: `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}?access_token=${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+        // httpEndpoint: `https://graphql.contentful.com/content/v1/spaces/e41op0ckhruz/explore?access_token=SnLR_zaGWeaZJkcJKEBE2gueCsM0XVJHsw91vGe-Lbo`,
+      },
     },
   },
 
