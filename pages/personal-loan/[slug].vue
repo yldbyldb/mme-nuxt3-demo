a217c5865deffc4ba5523307595a16ea8f324459
Bruce Dong <bruce.dong@societyone.com.au>
2022-11-18 13:16:50 +1100

    find a way to get data for templates

--- pages/personal-loan/[slug].vue
+++ pages/personal-loan/[slug].vue
@@ -1,7 +1,8 @@
 <template>
   <h1 class="text-3xl m-4">Personal Loan:</h1>
-  <h2 class="text-xl m-2">{{ data.pagepersonalLoan.calculator.header }}</h2>
+  <h2 class="text-xl m-2">{{ pageData.page }} ---> {{ pageData.calculator.header }} ---> {{ pageData.calculator.subheader }}</h2>
   <p class="text-l m-1"></p>
+  <img :src="pageData.headerWithImage.img.url" alt="">
 </template>
 <script setup>
 import { ref } from "vue";
@@ -10,17 +11,26 @@ const route = useRoute();
 const slug = ref(route.params.slug);
 const personalLoanContent = gql`
   query getPLContent($slug: String!) {
-    pagepersonalLoan (slug: $slug) {
-      calculator {
-        header
-        subheader
+    pagepersonalLoanCollection (where: {slug: $slug}){
+      items {
+        page
+        calculator {
+          header
+          subheader
+        }
+        headerWithImage {
+          img {
+            url
+          }
+        }
       }
     }
   }
 `
 const variables = { slug: slug.value }
 const { data }  = await useAsyncQuery(personalLoanContent, variables);
+const pageData = data.value.pagepersonalLoanCollection.items[0]
 console.log('====================================');
-console.log(variables);
+console.log(data.value.pagepersonalLoanCollection.items[0].page);
 console.log('====================================');
 </script>
